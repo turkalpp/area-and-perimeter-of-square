2645923358fd26258bbdfbdbded9abdc2df35519
turkalpp <71527921+turkalpp@users.noreply.github.com>
2021-11-03 22:14:11 +0300

    Add files via upload

---
+++ area and perimeter of square.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int edge;
+	cout<<"Enter an edge length: ";
+	cin>>edge;
+	cout<<"Perimeter:"<<edge*4<<endl;
+	cout<<"Area:"<<edge*edge;
+}
